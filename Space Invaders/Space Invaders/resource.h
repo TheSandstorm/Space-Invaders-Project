3337bd9a09c2f1e7c9d63e02c19be02fafe66671
Joza9080 <joshua.oc@gmail.com>
2018-11-20 00:23:56 +1300

    Revert "Merge branch 'master' of https://github.com/TheSandstorm/Space-Invaders-Project"
    This reverts commit d8fe390eb7668dcd9672a5f0af64cc571f65a879, reversing
    changes made to 7d5048adba5c3a76d67603ec5aa2d0b339f500e9.

---
+++ Space Invaders/Space Invaders/resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDB_EnemyBullet                 127
+#define IDB_Enemy1Mask                  129
+#define IDB_BITMAP3                     130
+#define IDB_Enemy1                      130
+#define IDB_Enemy2Mask                  131
+#define IDB_Enemy2                      132
+#define IDB_Enemy3Mask                  133
+#define IDB_Enemy3                      134
+#define IDB_Mothership                  135
+#define IDB_MothershipMask              136
+#define IDB_PlayerBullet                137
+#define IDB_Player                      138
+#define IDB_PlayerMask                  139
+#define IDB_BulletMask                  142
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        143
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
